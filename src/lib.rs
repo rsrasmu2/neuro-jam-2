ba50400e1c3e521976af612aec4d2d50ef7eedff
Robert Rasmussen <bobbyras90@gmail.com>
2024-12-27 18:58:34 -0700

    Fix assets not loading on web, simplify release workflow

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{asset::AssetMetaCheck, prelude::*};
 use camera::GameCameraPlugin;
 use game::GamePlugin;
 use screens::ScreensPlugin;
@@ -15,17 +15,22 @@ pub struct AppPlugin;
 impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Window {
-                    title: "Neuro Jam 2".to_string(),
-                    canvas: Some("#bevy".to_string()),
-                    fit_canvas_to_parent: true,
-                    prevent_default_event_handling: true,
+            DefaultPlugins
+                .set(AssetPlugin {
+                    meta_check: AssetMetaCheck::Never,
                     ..default()
-                }
-                .into(),
-                ..default()
-            }),
+                })
+                .set(WindowPlugin {
+                    primary_window: Window {
+                        title: "Neuro Jam 2".to_string(),
+                        canvas: Some("#bevy".to_string()),
+                        fit_canvas_to_parent: true,
+                        prevent_default_event_handling: true,
+                        ..default()
+                    }
+                    .into(),
+                    ..default()
+                }),
             asset_tracking::plugin,
             GameCameraPlugin,
             TilesPlugin,
